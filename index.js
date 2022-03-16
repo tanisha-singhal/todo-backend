5d7bc5a8038baf268142d5a618e2f746e00ecee7
tanisha-singhal <tanusinghal1000@gmail.com>
2022-03-16 16:35:26 +0530

    added cors

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+var cors = require('cors')
 const app = express();
-
+app.use(cors())
 
  app.use(express.json())
  app.use(express.static(path.join(__dirname, 'build')));

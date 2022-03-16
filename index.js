9c65c602cb0801b1290919db1633bd0e5459ade4
tanisha-singhal <tanusinghal1000@gmail.com>
2022-03-16 14:16:34 +0530

    modified

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-
+const path = require('path');
 const app = express();
 
 
  app.use(express.json())
- app.use(express.static('public'));
-
+ app.use(express.static(path.join(__dirname, 'build')));
+//  app.use(express.static('public'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 mongoose.connect(process.env.TODO_URI) .then(function (db) {
   console.log("db connected");

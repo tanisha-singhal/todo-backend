789a0ed1b6083d3050ba5026a597e9e47a3ddad1
tanisha-singhal <tanusinghal1000@gmail.com>
2022-03-18 14:24:05 +0530

    added cors

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const mongoose = require('mongoose');
 const path = require('path');
 var cors = require('cors')
 const app = express();
-app.use(cors())
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+app.use(cors(corsOptions))
 
  app.use(express.json())
  //app.use(express.static(path.join(__dirname, 'build')));
